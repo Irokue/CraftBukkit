57c88ef1bd7ce02707341280e94fea5c0c7af312
Rynzou <rynzou@sweetcraft.fr>
2012-07-12 23:43:42 +0200

    Remove

--- src/main/java/org/bukkit/craftbukkit/generator/InternalChunkGenerator.java
+++
@@ -1,8 +0,0 @@
-package org.bukkit.craftbukkit.generator;
-
-import net.minecraft.server.IChunkProvider;
-import org.bukkit.generator.ChunkGenerator;
-
-public abstract class InternalChunkGenerator extends ChunkGenerator implements IChunkProvider {
-
-}
